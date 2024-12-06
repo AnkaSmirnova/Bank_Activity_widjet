81c859909593c2195afbe4e0a01a626740e89433
Anna Smirnova <7259844@gmail.com>
2024-12-06 21:16:42 +0300

    save dz 9.1 and add gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+"""Программа запрашивает у пользователя ввод номера карты и
+номера счета, затем выводит маскированные значения."""
+
+user_card_number = input("Введите номер карты (16 цифр): ")
+
+user_account_number = input("Введите номер счета (20 цифр): ")
+
+if __name__ == "__main__":
+    print(get_mask_card_number(user_card_number))
+    print(get_mask_account(user_account_number))
