cc9edfd43fc9a7225fd6fcc85bd52698fb2ab5d9
Anna Smirnova <7259844@gmail.com>
2024-12-07 09:45:20 +0300

    linters code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
-"""Программа запрашивает у пользователя ввод номера карты и
+"""Программа запрашивает у пользователя ввод номера карты или
 номера счета, затем выводит маскированные значения."""
 
 
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,7 @@
-from src.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(info_cards: str) -> str:
     """Функция принимает строку, содержащую тип и номер (карты или счета).
@@ -6,27 +9,25 @@ def mask_account_card(info_cards: str) -> str:
     info_card = info_cards.split()
 
     if len(info_card) > 2:
-        type_card = info_card[0] + ' ' + info_card[1]
+        type_card = info_card[0] + " " + info_card[1]
 
     else:
         type_card, number_cards = info_card
 
-
     number_cards = info_card[-1]
 
-    if info_card[0].lower() in ['visa', 'maestro', 'mastercard']:
+    if info_card[0].lower() in ["visa", "maestro", "mastercard"]:
         masked_number = get_mask_card_number(number_cards)
         return f"{type_card} {masked_number}"
-    elif info_card[0].lower() == 'счет':
+    elif info_card[0].lower() == "счет":
         masked_number = get_mask_account(number_cards)
         return f"{type_card} {masked_number}"
     else:
         return "Неизвестный тип. Поддерживаемые типы: карта, счет."
 
-from datetime import datetime
 
 def get_date(date: str) -> str:
-    """ Функция принимает строку с датой в формате "YYYY-MM-DDTHH:MM:SS.ssssss"
+    """Функция принимает строку с датой в формате "YYYY-MM-DDTHH:MM:SS.ssssss"
     и возвращает строку с датой в формате "DD.MM.YYYY."""
     try:
         date_of_operation = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
